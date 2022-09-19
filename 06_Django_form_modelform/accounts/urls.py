fea6c914d220ce413ff5fa8ace2adec799ef39ee
김진호 <wlsghsla94@naver.com>
2022-09-20 01:20:43 +0900

    form.modelform

--- 06_Django_form_modelform/accounts/urls.py
+++ 06_Django_form_modelform/accounts/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('articles/', include('articles.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+
+    
 ]
